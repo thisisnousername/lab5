fcbad33824224bd0438c1b41451aa71cf5cb39ff
thisisnousername <markus.rosendahl@hhu.de>
2015-11-18 17:39:56 +0100

    edited randomwalk.cxx

--- randomwalk.cxx
+++ randomwalk.cxx
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <sstream>
 #include <cstdlib>
+#include <cmath>
 
 using namespace std;
 
@@ -9,8 +10,46 @@ struct colloid{
     double x,y;
 };
 
-void init(colloid* const c, const int N);
-void print(const colloid* const c, const int N, const string fname);
+void init(colloid* const c, const int N){
+    for(int i = 0; i < N; i++){
+	c[i].x = 0;
+	c[i].y = 0;
+    }
+}
+
+void print(const colloid* const c, const int N, const string fname){
+    ofstream out(fname.c_str());
+    for(int i = 0; i < N; i++)
+	out << c[i].x << "\t" << c[i].y << endl;
+    out.close();
+}
+
+void randomize(int* rx, int *ry, const int N){
+	for(int i=0; i<N; i++){
+		rx[i] = int((double(rand())/RAND_MAX)*3)-1;
+		ry[i] = int((double(rand())/RAND_MAX)*3)-1;
+	}
+}
+
+void push(colloid* const c, int*rx, int* ry, const int N, const double step){
+	for(int i=0; i<N; i++){
+		c[i].x += step*rx[i];
+		c[i].y += step*ry[i];
+	}
+}
+
+void evaluate(const colloid* const c, double& meanx, double& meany, double& var, const int N){
+	meanx=0; meany=0; var=0;
+	for(int i=0; i<N; i++){
+		meanx += c[i].x;
+		meany += c[i].y;
+	}
+	meanx /= N;
+	meany /= N;
+	for(int i=0; i<N; i++)
+		var += pow(c[i].x-meanx, 2.0)+pow(c[i].y-meany, 2.0);
+	var /= N;
+}
 
 int main(void){
   
@@ -44,9 +83,9 @@ int main(void){
     
     for(int i = 1; i <= Nfiles; i++){
 	for(int j = 0; j < Nsubsteps; j++){
-	    // call to function which randomly sets up rx and ry
-	    // call to function which pushes all colloids according to rx and ry
-	    // call to function which evaluates statistics
+		randomize(rx, ry, N);			// call to function which randomly sets up rx and ry
+		push(c, rx, ry, N, step);		// call to function which pushes all colloids according to rx and ry
+		evaluate(c, meanx, meany, var, N);	// call to function which evaluates statistics
 	    stat << (i-1)*Nsubsteps+j << "\t" << meanx << "\t";
 	    stat << meany << "\t" << var << endl;
 	}
@@ -58,18 +97,4 @@ int main(void){
     stat.close();			// close statistics file
     delete[] rx,ry,c;			// delete dynamically allocated arrays
     return 0;
-}
-
-void init(colloid* const c, const int N){
-    for(int i = 0; i < N; i++){
-	c[i].x = 0;
-	c[i].y = 0;
-    }
-}
-
-void print(const colloid* const c, const int N, const string fname){
-    ofstream out(fname.c_str());
-    for(int i = 0; i < N; i++)
-	out << c[i].x << "\t" << c[i].y << endl;
-    out.close();
 }
