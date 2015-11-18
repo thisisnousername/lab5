a5b87bcaa9832ac46e5c020731f9b64d643f87e7
Friedrich Schluck <fschluck@tp1.uni-duesseldorf.de>
2015-11-18 13:39:24 +0100

    changed readme + src file

--- randomwalk.cxx
+++ randomwalk.cxx
@@ -10,54 +10,53 @@ struct colloid{
 };
 
 void init(colloid* const c, const int N);
-void conditions(int* const rx, int* const ry, const int N);
-void pusher(colloid* const c, const int* const rx, const int* const ry, const double step, const int N);
 void print(const colloid* const c, const int N, const string fname);
-void statistics(double& meanx, double& meany, double& var, const colloid* const c, const int N);
-
 
 int main(void){
   
-    const int N  = 50;
-    int*      rx = new int[N];
-    int*      ry = new int[N];
-    colloid*  c  = new colloid[N];
+    int N  = 50;
+    cout << "Right now # of colloids is set to " << N << ", how many do you want?" << endl;
+    cin  >> N;
     
-    double meanx, meany, var;
-    ofstream stat("statistics.dat");
+    colloid*  c  = new colloid[N];	// dynamically allocate memory for N particles
+    int*      rx = new int[N];		// dynamically allocate memory for movement-conditions in x direction
+    int*      ry = new int[N];		// dynamically allocate memory for movement-conditions in y direction
     
-    const double step = 0.1;
+    double meanx, meany, var;		// statistics
+    ofstream stat("statistics.dat");	// ... to be printed out here
     
-    init(c, N);
+    const double step = 0.1;		// step size
     
-    stringstream s;
-    string       fname = "rwalk";
+    init(c, N);				// initialize all colloids
     
-    const int Nsteps = 100;
-    const int Nfiles = 10;
-    int Nsubsteps    = Nsteps / Nfiles;
+    stringstream s;			// set up stringstream -> multiple output files
+    string       fname = "rwalk";	// basic name tag for multiple output files
     
-    s.str("");
-    s << fname << "_0.dat";
-    print(c, N, s.str());
+    const int Nsteps = 100;		// total # of steps
+    const int Nfiles = 10;		// total # of output files
+    int Nsubsteps    = Nsteps / Nfiles; // resulting substeps
     
-    srand(time(NULL));
+    s.str("");				// empty stringstream
+    s << fname << "_0.dat";		// fill in string for initial file
+    print(c, N, s.str());		// print initial positions
+    
+    srand(time(NULL));			// randomize random numbers
     
     for(int i = 1; i <= Nfiles; i++){
 	for(int j = 0; j < Nsubsteps; j++){
-	    conditions(rx, ry, N);
-	    pusher(c, rx, ry, step, N);
-	    statistics(meanx, meany, var, c, N);
+	    // call to function which randomly sets up rx and ry
+	    // call to function which pushes all colloids according to rx and ry
+	    // call to function which evaluates statistics
 	    stat << (i-1)*Nsubsteps+j << "\t" << meanx << "\t";
 	    stat << meany << "\t" << var << endl;
 	}
-	s.str("");
-	s << fname << "_" << i << ".dat";
-	print(c, N, s.str());
+	s.str("");						// preparations for printing...
+	s << fname << "_" << i << ".dat";			// ... colloid positions
+	print(c, N, s.str());					
     }
     
-    stat.close();
-    delete[] rx,ry,c;
+    stat.close();			// close statistics file
+    delete[] rx,ry,c;			// delete dynamically allocated arrays
     return 0;
 }
 
@@ -68,39 +67,9 @@ void init(colloid* const c, const int N){
     }
 }
 
-void conditions(int* const rx, int* const ry, const int N){
-    for(int i = 0; i < N; i++){
-	rx[i] = int(2*double(rand())/RAND_MAX + 0.5) - 1;
-	ry[i] = int(2*double(rand())/RAND_MAX + 0.5) - 1;
-    }
-}
-
-void pusher(colloid* const c, const int* const rx, const int* const ry, const double step, const int N){
-    for(int i = 0; i < N; i++){
-	c[i].x += rx[i]*step;
-	c[i].y += ry[i]*step;
-    }    
-}
-
 void print(const colloid* const c, const int N, const string fname){
     ofstream out(fname.c_str());
     for(int i = 0; i < N; i++)
 	out << c[i].x << "\t" << c[i].y << endl;
     out.close();
-}
-
-void statistics(double& meanx, double& meany, double& var, const colloid* const c, const int N){
-    meanx = 0;
-    meany = 0;
-    for(int i = 0; i < N; i++){
-	meanx += c[i].x;
-	meany += c[i].y;
-    }
-    meanx /= N;
-    meany /= N;
-    
-    var = 0;
-    for(int i = 0; i < N; i++)
-	var += (c[i].x-meanx)*(c[i].x-meanx) + (c[i].y-meany)*(c[i].y-meany);
-    var /= N;
 }
